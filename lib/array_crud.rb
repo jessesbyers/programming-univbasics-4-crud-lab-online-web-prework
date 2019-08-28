7737afe767bb14cc92a70544721df56b35ab2f49
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-28 13:45:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  cousins = ["wow", "Luke", "Ryan", "Jake", "Griffin", "arrays!"]
+  cousins[0]
 end
 
 def retrieve_last_element_from_array(array)

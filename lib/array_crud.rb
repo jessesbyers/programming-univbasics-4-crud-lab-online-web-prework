08e7902e0c58d6e19f628328757128646f7a7b79
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-28 13:32:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   cousins = ["Luke", "Ryan", "Jake", "Griffin"]
-  cousins << "Alika"
+  cousins << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
